4bd8da7130e7db033a4bfc00e6e64a20e679033e
Keila Amada <keila.amadap@hotmail.com>
2023-10-16 19:03:08 -0300

    feat: adicionar rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListagemCarros from "./components/Listagem/ListagemCarros";
+import Form from "./components/Carros/Form/Form";
+import { NavProvider } from "./context/NavContext";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <ListagemCarros />;
+  return (
+    <Router>
+      <NavProvider>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<ListagemCarros />} />
+          <Route path="/novo" element={<Form />} />
+        </Routes>
+      </NavProvider>
+    </Router>
+  );
 }
 
 export default App;
