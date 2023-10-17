c0b6612dd38a26a86b7cc343f329ef7140580e40
Keila Amada <keila.amadap@hotmail.com>
2023-10-17 12:08:14 -0300

    feat: contexto para gerenciamento de estado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListagemCarros from "./components/Listagem/ListagemCarros";
 import Form from "./components/Carros/Form/Form";
-import { NavProvider } from "./context/NavContext";
-import Navbar from "./components/Navbar/Navbar";
+import { MyContextProvider } from "./context/CarContext";
 
 function App() {
   return (
-    <Router>
-      <NavProvider>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<ListagemCarros />} />
-          <Route path="/novo" element={<Form />} />
-        </Routes>
-      </NavProvider>
-    </Router>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        width: "1000px",
+      }}
+    >
+      <MyContextProvider>
+        <ListagemCarros />
+        <Form />
+      </MyContextProvider>
+    </div>
   );
 }
 
--- src/components/Carros/Carros.jsx
+++ src/components/Carros/Carros.jsx
@@ -1,27 +1,27 @@
 import * as S from "./styles";
 
-const Carros = ({ modelo, ano, marca, valor, cor }) => {
-  const formatarTexto = (texto) => {
-    return texto.charAt(0).toUpperCase() + texto.slice(1).toLowerCase();
+const Carros = ({ model, year, brand, value, color }) => {
+  const formatText = (text) => {
+    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
   };
 
   return (
     <S.Div>
       <S.Paragraph>
         Ano de Lançamento:
-        <S.Info>{ano}</S.Info>
+        <S.Info>{year}</S.Info>
       </S.Paragraph>
       <S.Paragraph>
         Valor:
-        <S.Info>R${valor.toFixed(3).replace(".", ",")}</S.Info>
+        <S.Info>R${value.toFixed(3).replace(".", ",")}</S.Info>
       </S.Paragraph>
       <S.Paragraph>
         Nome/Modelo:
-        <S.Info>{formatarTexto(modelo)}</S.Info>
+        <S.Info>{formatText(model)}</S.Info>
       </S.Paragraph>
       <S.Paragraph>
         Cor:
-        <S.Info>{formatarTexto(cor)}</S.Info>
+        <S.Info>{formatText(color)}</S.Info>
       </S.Paragraph>
     </S.Div>
   );
